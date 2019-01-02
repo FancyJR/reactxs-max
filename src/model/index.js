0fe937f845125e6d6d930b0b0586e609f80ad5cc
yun.shi <1817893777@qq.com>
2019-01-02 11:33:35 +0800

    疯狂redux

---
+++ src/model/index.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+import reducers, { initialState } from './reducers';
+
+let enhancer = applyMiddleware(thunk);
+if (process.env.NODE_ENV === 'development') {
+  enhancer = compose(
+    applyMiddleware(thunk, createLogger())
+  )
+}
+
+let store = createStore(
+  reducers,
+  initialState,
+  enhancer
+)
+
+if (process.env.NODE_ENV === 'development') {
+  if(module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = require('./reducers').default;
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+}
+
+export default store;
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/axios/index.d.ts" />
 /// <reference path="globals/jest/index.d.ts" />
+/// <reference path="globals/redux-thunk/index.d.ts" />
 /// <reference path="globals/webpack-dev-server/index.d.ts" />
